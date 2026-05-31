a60c7956e34743dd94cae40c9a5064850331af10
F1D02310011_Irfan Jayadi <irfanjayadi2004@gmail.com>
2026-05-31 09:36:52 +0800

    buat database nya berfungsi

--- database.py
+++ database.py
@@ -2,15 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+# PERHATIKAN: Domain pooler menggunakan port 6543
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.cyiofgibzuphlglwicra:irfan25postgress@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres"
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.cyiofgibzuphlglwicra:irfan25postgress@db.cyiofgibzuphlglwicra.supabase.co:5432/postgres"
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+# Tambahkan pool_pre_ping=True agar koneksi pooler tidak terputus saat membuat tabel
+engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Fungsi untuk membuka koneksi setiap kali API dipanggil
 def get_db():
     db = SessionLocal()
     try:
